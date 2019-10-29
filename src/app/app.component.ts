d5a0809481247fdc14cedc0fce1f8bd0fcd32031
Mel Raymond Salma <melagm3@gmail.com>
2019-10-30 00:11:57 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from './_services';
+import { User } from './_models';
+
+import './_content/app.less';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+    title = "bookRental";
+    description = "Book Rental Website";
+
+    itemvalue: '';
+    items: Observable<any[]>;
+
+    currentUser: User;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        public db: AngularFireDatabase
+    ) {
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+        this.items = db.list("items").valueChanges();
+    }
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDubfMOXA4IcxEZ6EjZY40Qh_9XIHahN7s",
+        authDomain: "bookrental-c6842.firebaseapp.com",
+        databaseURL: "https://bookrental-c6842.firebaseio.com",
+        projectId: "bookrental-c6842",
+        storageBucket: "bookrental-c6842.appspot.com",
+        messagingSenderId: "847303312034",
+        appId: "1:847303312034:web:c70310e76475cad5d2cdfb",
+        measurementId: "G-1FS5WN9YSK"
+    }
+};
